9c1587353cf04a81392b6f339e42e825253241e6
martineho <martineolsenn@hotmail.com>
2022-11-03 22:40:15 +0700

    Update index.js

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,6 @@
 import { Heading } from "../../components/layout/Headings";
 import Container from 'react-bootstrap/Container';
 import Navigation from "../../components/layout/Navigation";
-import getDetails from "../../components/profile/UserDetails";
 
 export default function Profile() {
   return (
