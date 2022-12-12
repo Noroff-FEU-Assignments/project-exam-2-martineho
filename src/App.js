4751d1490b6c5e9c0c095db84416d0a21138a792
martineho <martineolsenn@hotmail.com>
2022-12-12 13:47:43 +0800

    add back helmet provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import AppRoutes from './utils/Routes'
-
+import { HelmetProvider } from 'react-helmet-async';
 
 export default function App() {
   return (
-
+    <HelmetProvider>
       <AppRoutes /> 
-
+   </HelmetProvider>
 );}
