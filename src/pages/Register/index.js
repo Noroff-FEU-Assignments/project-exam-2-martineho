a1129826556c3e8f644034f089f6948dab023a17
martineho <martineolsenn@hotmail.com>
2022-12-12 13:42:40 +0800

    remove helmet provider

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -3,15 +3,11 @@ import { BigHeading } from "../../components/layout/Headings";
 import Footer from "../../components/layout/Footer";
 import BigParagraph from "../../components/layout/Paragraphs";
 import RegisterForm from "../../components/features/auth/RegisterForm";
-import SEO from "../../utils/SEO";
+
 
 export default function Register() {
   return (
     <>
-    <SEO
-    title='aesocial - Become a member'
-    description='A mindful social media platform. Content with substance. Find likeminded people and be inspired.'
-    type='social media' />
 
     <Container>
       <div className="login-background"></div>
